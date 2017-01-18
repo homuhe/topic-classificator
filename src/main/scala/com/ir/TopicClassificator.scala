99239da83fb0f33d39717fa57b3ce0f975d04e4d
homuhe <homuhe@gmail.com>
2017-01-18 16:53:27 +0100

    Initial commit

---
+++ src/main/scala/com/ir/TopicClassificator.scala
@@ -0,0 +1,20 @@
+package com.ir
+
+/** Author:       Alexander Hartmann,
+  *               Holger Muth-Hellebrandt
+  *
+  * Task:         Assignment 4
+  * Description:  Document Topic Classification by SVM
+  */
+
+
+object TopicClassificator {
+
+  /**
+    * Main method
+    */
+  def main(args: Array[String]): Unit = {
+    println("Harambe!")
+  }
+
+}
